04582f211baf4d2db19cd32cb4a807a9ba0305e1
arthmn <162844089+arthmn@users.noreply.github.com>
2024-03-09 19:50:40 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,13 @@
+/* Lab.1
+ * Exemplo 1*/
+
+#include <stdio.h>
+
+int main() {
+  //Imprimir algumas mensagens na tela
+  printf("Mackenzie - FCI\n");
+  printf("Algoritmos e ");
+  printf("Programação II\n");
+
+  return 0;
+}
